e6f8a178c4b6b06dec7a78f07a4c0405d8d4a148
yousef <yzabalawy@gmail.com>
2024-09-25 02:41:09 +0300

    deleteAction

--- actions/delete-board/types.ts
+++ actions/delete-board/types.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
-import { DeleteBoard } from "./schema";
+import { DeleteBoardSchema } from "./schema";
 import { ActionState } from "@/lib/create-safe-action";
 import { Board } from "@prisma/client";
 
 
 
-export type InputType = z.infer<typeof DeleteBoard>
+export type InputType = z.infer<typeof DeleteBoardSchema>
 export type ReturnType = ActionState<InputType , Board>
